fb25452d7efdfeccf1b0fa637ce2257bd86cf64b
朱孔信 <zeoxcine@Zkx-2.local>
2024-08-17 23:44:00 +0800

    Combat Polish / Fix RespawningCoin Prefab(Add a component fogotten to be added last time)

--- Assets/Scripts/Core/Player/ProjectileLauncher.cs
+++ Assets/Scripts/Core/Player/ProjectileLauncher.cs
@@ -9,6 +9,7 @@ public class ProjectileLauncher : NetworkBehaviour
 {
     [Header("References")]
     [SerializeField]private InputReader inputReader;
+    [SerializeField]private CoinWallet coinWallet;
     [SerializeField]private Transform projectileSpawnPoint;
     [SerializeField]private GameObject serverProjectilePrefab;
     [SerializeField]private GameObject clientProjectilePrefab;
@@ -19,10 +20,11 @@ public class ProjectileLauncher : NetworkBehaviour
     [SerializeField]private float projectileSpeed;
     [SerializeField]private float fireRate;
     [SerializeField]private float muzzleFlshDuration;
+    [SerializeField]private int costToFire;
     
 
     private bool shouldFire;
-    private float previousFireTime;
+    private float timer;
     private float muzzleFlashTimer;
 
     public override void OnNetworkSpawn()
@@ -49,14 +51,20 @@ public class ProjectileLauncher : NetworkBehaviour
 
         if(!IsOwner){return;}
 
+        if(timer > 0){
+            timer -= Time.deltaTime;
+        }
+
         if(!shouldFire){return;}
 
-        if(Time.time < (1 / fireRate) + previousFireTime){return;}
+        if(timer > 0){return;}
 
-        previousFireTime = Time.time;
+        if(coinWallet.TotalCoins.Value < costToFire){return;}
 
         PrimaryFireServerRpc(projectileSpawnPoint.position, projectileSpawnPoint.up);
         SpawnDummyProjectile(projectileSpawnPoint.position, projectileSpawnPoint.up);
+
+        timer = 1 / fireRate;
     }
 
     private void HandlePrimaryFire(bool obj)
@@ -66,6 +74,11 @@ public class ProjectileLauncher : NetworkBehaviour
 
     [ServerRpc]
     private void PrimaryFireServerRpc(Vector3 spawnPos, Vector3 direction){
+
+        if(coinWallet.TotalCoins.Value < costToFire){return;}
+
+        coinWallet.SpendCoins(costToFire);
+
         GameObject projectileInstance = Instantiate(
             serverProjectilePrefab,
             spawnPos,
