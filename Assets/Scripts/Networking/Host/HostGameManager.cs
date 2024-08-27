52e9a16a77f352c0ee123a03ba86a886ccf789e5
朱孔信 <zeoxcine@Zkx.mshome.net>
2024-08-28 00:03:20 +0800

    Application Controller

---
+++ Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class HostGameManager
+{
+    public async Task InitAsync(){
+        //Authenticate player
+    }
+}
